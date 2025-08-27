c41c07e945768ba34f0805e162c84144dbf50fbd
“goktug”-aygun <goktug.aygun@sabanciuniv.edu>
2025-08-27 13:05:14 +0300

    Card Component Beginning

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Card from "./components/Card";
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <div className="row">
+      <Card
+        title="Master's Education at KU Leuven"
+        image="src/assets/images/projects/KU_Leuven_logo.png"
+        description="I am pursuing my Master's Degree at KU Leuven with a strong focus on Artificial Intelligence and Machine Learning. I am eager to sharpen my skills and improve my expertise by contributing to projects and research in these fields. My goal is to apply AI-driven approaches to real-world problems and develop efficient, impactful solutions."
+        buttonText="Learn More"
+        interval="Sep. 2025 - Exp. Jun. 2027"
+      />
+    </div>      
     </>
   )
 }
