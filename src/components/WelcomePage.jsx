081883da82889005e5e2839a11ae0f684c52b8e1
“goktug”-aygun <goktug.aygun@sabanciuniv.edu>
2025-08-29 17:34:02 +0300

    Welcome Page

---
+++ src/components/WelcomePage.jsx
@@ -0,0 +1,75 @@
+export default function WelcomePage() {
+  return (
+    <section id="home-page" className="page odd pt-5">
+      <div className="welcome-box container-fluid d-flex flex-xl-row flex-column">
+        {/* Memoji */}
+        <div className="col-xl-6 d-flex justify-content-center">
+          <img
+            className="memoji-img"
+            src="images/memoji-wave.png"
+            alt="Goktug waving"
+          />
+        </div>
+
+        {/* Landing box */}
+        <div className="landing-box col-xl-6 d-flex flex-column align-items-center justify-content-center">
+          <span
+            style={{ fontSize: "50px" }}
+            id="landing-text"
+            className="landing-text p-4"
+          >
+            Hey! It's Goktug.
+          </span>
+
+          <a
+            href="Goktug_Aygun_CV.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="downloadCV home_button"
+          >
+            Get My CV Here!
+          </a>
+
+          {/* Education Box*/}
+          <div className="education-box mt-4">
+            <div className="education-box mt-4 w-100">
+              {/* MSc */}
+              <div className="education-item p-3 mb-3 border rounded bg-light w-100">
+                <h5>MSc</h5>
+                <p className="fw-bold mb-1">KU Leuven</p>
+                <div className="d-flex justify-content-center">
+                  <p className="fw-bold mb-1 me-2 ">Leuven, Belgium</p>{" "}
+                  <img
+                    className="lang-icon"
+                    src={"/images/flags/belgium.png"}
+                    alt={"belgian Flag"}
+                  />
+                </div>
+                <p className="mb-1">Artificial Intelligence</p>
+                <p className="text-muted mb-1">Sep. 2025 - Exp. Jun. 2027</p>
+                {/* <p className="mb-0">GPA: —</p> N/A */}
+              </div>
+
+              {/* BSc */}
+              <div className="education-item p-3 mb-3 border rounded bg-light w-100">
+                <h5>BSc</h5>
+                <p className="fw-bold mb-1">Sabancı University</p>
+                <div className="d-flex justify-content-center">
+                  <p className="fw-bold mb-1 me-2 ">Istanbul, Turkey</p>{" "}
+                  <img
+                    className="lang-icon"
+                    src={"/images/flags/turkey.png"}
+                    alt={"Turkish Flag"}
+                  />
+                </div>
+                <p className="mb-1">Computer Science and Engineering</p>
+                <p className="text-muted mb-1">Sep. 2021 - Jun. 2025</p>
+                <p className="mb-0">GPA: 3.78/4.00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
