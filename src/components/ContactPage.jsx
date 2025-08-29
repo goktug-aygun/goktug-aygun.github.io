37bcbdaf9e3962128b760d1f0d547fe00b8252f8
“goktug”-aygun <goktug.aygun@sabanciuniv.edu>
2025-08-29 17:26:34 +0300

    Added Contact Page With Toast Notifications

---
+++ src/components/ContactPage.jsx
@@ -0,0 +1,182 @@
+import ContactPageBox from "./ContactPageBox";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+import "../toast.css";
+import "../animations.css";
+
+export default function ContactPage() {
+  // Variables for Email.js call
+  const [vst_name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+
+  // References for Form Boxes
+  const nameRef = useRef(null);
+  const msgRef = useRef(null);
+  const mailRef = useRef(null);
+  const phoneRef = useRef(null);
+
+  const [toast, setToast] = useState({ msg: "", type: "" });
+
+  // will transfer to json locales soon
+  let name_warning =
+    "<i class='bi bi-exclamation-triangle'></i>Please write your name.";
+  let mail_warning =
+    "<i class='bi bi-exclamation-triangle'></i>Please write your email.";
+  let message_warning =
+    "<i class='bi bi-exclamation-triangle'></i>Please write your message.";
+  let valid_warning =
+    "<i class='bi bi-hand-thumbs-up'></i>Thank you for your mail! I will get back to you soon.";
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    validateForm();
+  }
+
+  function validateForm() {
+    if (vst_name.trim() === "") {
+      nameRef.current.focus();
+      show(name_warning, "invalid");
+      return false;
+    }
+
+    if (email.trim() === "") {
+      mailRef.current.focus();
+      show(mail_warning, "invalid");
+      return false;
+    }
+
+    if (message.trim() === "") {
+      msgRef.current.focus();
+      show(message_warning, "invalid");
+      return false;
+    }
+
+    show(valid_warning, "valid");
+    sendMail();
+    return true;
+  }
+
+  function sendMail() {
+    const params = { name: vst_name, email, number: phone, msg: message };
+
+    console.log("Sending mail with params:", params);
+
+    emailjs
+      .send(
+        "service_qtuotuz", // Service ID
+        "template_a3xdol8", // Template ID
+        params,
+        "KtdNDRoFA_1mMpx_w" // Public Key
+      )
+      .then(
+        (res) => {
+          setName("");
+          setEmail("");
+          setPhone("");
+          setMessage("");
+        },
+        (err) => {}
+      );
+  }
+
+  function show(msg, type = "") {
+    setToast({ msg, type });
+
+    setTimeout(() => {
+      setToast({ msg: "", type: "" });
+    }, 3000);
+  }
+
+  return (
+    <section id="contact-page" className="odd page">
+      <div className="text-center">
+        <h1 className="page-title">Contact</h1>
+        <p className="lead pt-1">
+          Please feel free to reach out if you have any questions
+          <br />I would love to hear your feedbacks.
+        </p>
+        <img
+          src="images/memoji-contact.png"
+          style={{ height: "25vh", objectFit: "contain", margin: 0 }}
+          alt="contact memoji"
+        />
+      </div>
+
+      <div className="row justify-content-center">
+        <div className="col-lg-6">
+          <form onSubmit={handleSubmit}>
+            {/* Name */}
+            <ContactPageBox
+              className="no-bg-change"
+              id="name"
+              BoxText="Your Name"
+              type="text"
+              value={vst_name}
+              onChange={(e) => setName(e.target.value)}
+              ref={nameRef}
+            />
+
+            {/* Email */}
+            <ContactPageBox
+              className="no-bg-change"
+              id="email"
+              BoxText="Your Email"
+              type="email"
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              ref={mailRef}
+            />
+
+            {/* Phone Number */}
+            <ContactPageBox
+              className="no-bg-change"
+              id="number"
+              BoxText="Your Phone Number (Not Required)"
+              type="text"
+              inputMode="numeric"
+              pattern="[0-9]+"
+              value={phone}
+              ref={phoneRef}
+              onChange={(e) => {
+                const onlyNumbers = e.target.value.replace(/\D/g, "");
+                setPhone(onlyNumbers);
+              }}
+            />
+
+            {/* Message */}
+            <ContactPageBox
+              className="no-bg-change"
+              id="msg"
+              BoxText="Your Message"
+              type="text"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              ref={msgRef}
+            />
+
+            <div className="text-center">
+              <button
+                type="submit"
+                className="btn btn-outline-dark w-100 text-center mx-auto"
+              >
+                Send
+              </button>
+            </div>
+          </form>
+          <div id="toast-box">
+            {toast.msg && (
+              <div
+                className={`toast ${toast.type}`}
+                dangerouslySetInnerHTML={{ __html: toast.msg }}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/ContactPageBox.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ContactPageBox = React.forwardRef(
+  ({ BoxText, id, className = "", ...props }, ref) => {
+    return (
+      <div className="form-floating">
+        <input
+          id={id}
+          className={`form-control my-2 my-md-3 my-lg-4 ${className}`}
+          placeholder={BoxText}
+          ref={ref}
+          {...props}
+        />
+        <label htmlFor={id} className="form-label">
+          {BoxText}
+        </label>
+      </div>
+    );
+  }
+);
+
+export default ContactPageBox;
